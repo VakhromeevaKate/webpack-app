af2eeb64f3830a9ba8eeff7d2af532391d10a5d0
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-02 19:14:16 +0300

    Some bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
     build: PATH.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         'index': PATHS.source + '/pages/index/index.js',
         'blog': PATHS.source + '/pages/blog/blog.js'
@@ -38,5 +38,26 @@ module.exports = {
             }
         ]
         
+    },
+    
+};
+
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only',
+        port: 9080
     }
 };
+
+module.exports = function(env) {
+    if (env === 'production'){
+        return common;
+    }
+    if (env === 'development'){
+        return Object.assign(
+            {},
+            common,
+            developmentConfig
+        )
+    }
+}
